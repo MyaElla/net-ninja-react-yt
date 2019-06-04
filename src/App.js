be6cc58d9978515a0f7f81974b94d40bf787d240
Mihaela Ciobanu <mihaela.ciobanu03@gmail.com>
2019-06-04 12:54:36 +0100

    create new state pass funct as prop

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
 import Child from './Child'
+import AddHuman from "./AddHuman";
 
 class App extends Component {
   state = {
     ninjas: [
-      { name: 'Jonas', age: 30, pet: 'dog', id: 1 },
-      { name: 'Nick', age: 25, pet: 'turtle', id: 2 },
-      { name: 'Zoe', age: 17, pet: 'cat', id: 3 }
+      { name: "Jonas", age: 30, pet: "dog", id: 1 },
+      { name: "Nick", age: 25, pet: "turtle", id: 2 },
+      { name: "Zoe", age: 17, pet: "cat", id: 3 }
     ]
   };
+
+  addHuman = human => {
+    human.id = Math.random();
+
+    let ninjasNew = [...this.state.ninjas, human];
+    this.setState(
+      {
+        ninjas: ninjasNew
+      },
+      () => {
+        console.log(this.state.ninjas);
+      }
+    );
+  };
+
   render() {
+    console.log("addNinja", this.addNinja);
     return (
       <div className="App">
         <h1>class React app</h1>
         <Child ninjas={this.state.ninjas} />
+        <AddHuman addNinja={this.addHuman} />
       </div>
     );
   }
