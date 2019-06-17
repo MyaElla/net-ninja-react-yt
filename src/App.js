f6d8de2110550db4b820372e8736472f5e5cf956
Mihaela Ciobanu <mihaela.ciobanu03@gmail.com>
2019-06-17 11:18:24 +0100

    show only one route, instead of matching and showing several, using Switch tag

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom'
+import { Route, BrowserRouter, Switch } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About'
@@ -12,10 +12,12 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Navbar />
-        <Route exact path='/' component={Home} />
-        <Route path='/about' component={About} />
-        <Route path='/contact' component={Contact} />
-        <Route path='/:post_id' component={Post} />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/:post_id' component={Post} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
