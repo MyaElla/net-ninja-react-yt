c2902d46a809b311c86b2df2f359e4b4544e2964
Mihaela Ciobanu <mihaela.ciobanu03@gmail.com>
2019-06-17 12:29:27 +0100

    create custom action generator for a DELETE action.

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,7 @@
 
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { deletePost } from '../actions/postActions'
 
 class Post extends Component {
     handleClick = () => {
@@ -40,9 +41,7 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        deletePost: (id) => dispatch(
-            { type: 'DELETE_POST', id: id } // the action
-            )
+        deletePost: (id) => dispatch(deletePost(id))
     }
 }
 
